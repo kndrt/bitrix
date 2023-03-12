ccfee814fd5676b7fc6914eda699f29fc5df9678
= <=>
2023-03-12 15:59:23 +0300

    task2

---
+++ include/logo.php
@@ -0,0 +1,2 @@
+<a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span
+class="text-danger">.</span></strong></a>
--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -45,16 +45,37 @@
         <div class="row align-items-center">
           <div class="col-6 col-md-6">
             <p class="mb-0">
-              <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span
-                  class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
-              <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span
-                  class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
+            <?$APPLICATION->IncludeComponent(
+              "bitrix:main.include",
+              "",
+              Array(
+                "AREA_FILE_SHOW" => "file",
+                "PATH" => "/include/phone.php",
+                "EDIT_TEMPLATE" => ""
+                )
+            );?>
+            <?$APPLICATION->IncludeComponent(
+              "bitrix:main.include",
+              "",
+              Array(
+                "AREA_FILE_SHOW" => "file",
+                "PATH" => "/include/email.php",
+                "EDIT_TEMPLATE" => ""
+                )
+            );?>           
+            
             </p>
           </div>
           <div class="col-6 col-md-6 text-right">
-            <a href="#" class="mr-3"><span class="text-black icon-facebook"></span></a>
-            <a href="#" class="mr-3"><span class="text-black icon-twitter"></span></a>
-            <a href="#" class="mr-0"><span class="text-black icon-linkedin"></span></a>
+          <?$APPLICATION->IncludeComponent(
+              "bitrix:main.include",
+              "",
+              Array(
+                "AREA_FILE_SHOW" => "file",
+                "PATH" => "/include/social_media.php",
+                "EDIT_TEMPLATE" => ""
+                )
+            );?>                
           </div>
         </div>
       </div>
@@ -64,8 +85,17 @@
       <div class="container py-1">
         <div class="row align-items-center">
           <div class="col-8 col-md-8 col-lg-4">
-            <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span
-                    class="text-danger">.</span></strong></a></h1>
+            <h1 class="">          
+              <?$APPLICATION->IncludeComponent(
+              "bitrix:main.include",
+              "",
+              Array(
+                "AREA_FILE_SHOW" => "file",
+                "PATH" => "/include/logo.php",
+                "EDIT_TEMPLATE" => ""
+                )
+            );?>     
+            </h1>
           </div>
           <div class="col-4 col-md-4 col-lg-8">
             <nav class="site-navigation text-right text-md-right" role="navigation">
@@ -75,6 +105,7 @@
 
               <ul class="site-menu js-clone-nav d-none d-lg-block">
                 <li class="active">
+                  
                   <a href="index.html">Home</a>
                 </li>
                 <li class="has-children">
